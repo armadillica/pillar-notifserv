9a89cf501f7b67679f3ec148359673627458324c
Sybren A. Stüvel <sybren@stuvel.eu>
2016-05-17 04:45:01 +0200

    Initial checkin.

---
+++ pillar-notifserv.go
@@ -0,0 +1,117 @@
+// Golang HTML5 Server Side Events Example
+//
+// Run this code like:
+//  > go run server.go
+//
+// Then open up your browser to http://localhost:8000
+// Your browser must support HTML5 SSE, of course.
+
+// Source: https://github.com/kljensen/golang-html5-sse-example/blob/master/server.go
+
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"github.com/armadillica/pillar-notifserv/pillar"
+	"gopkg.in/mgo.v2"
+	"encoding/json"
+)
+
+
+type SSE struct{
+	session *mgo.Session
+}
+
+func (self *SSE) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.RemoteAddr, "Channel started at", r.URL.Path)
+
+	// Make sure that the writer supports flushing.
+	//
+	f, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+
+	// Listen to the closing of the http connection via the CloseNotifier
+	close_notifier, ok := w.(http.CloseNotifier)
+	if !ok {
+		http.Error(w, "Cannot stream", http.StatusInternalServerError)
+		return
+	}
+
+	notifications := pillar.ForwardNotifications(self.session)
+
+	// Set the headers related to event streaming.
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	for {
+		select {
+		case <-close_notifier.CloseNotify():
+			log.Println(r.RemoteAddr, "Connection closed.")
+			return
+		case n := <-notifications:
+			msg, err := json.Marshal(n)
+			if err != nil {
+				log.Println(r.RemoteAddr, "Unable to marshal notification as JSON:", err)
+				break
+			}
+			fmt.Fprintf(w, "data: Message: %s\n\n", msg)
+			f.Flush()
+		}
+	}
+
+	// Done.
+	log.Println(r.RemoteAddr, "Finished HTTP request at", r.URL.Path)
+}
+
+// Handler for the main page, which we wire up to the
+// route at "/" below in `main`.
+//
+func MainPageHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Did you know Golang's ServeMux matches only the
+	// prefix of the request URL?  It's true.  Here we
+	// insist the path is just "/".
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	// Read in the template with our SSE JavaScript code.
+	t, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		log.Fatal("dude, error parsing your template:", err)
+	}
+
+	// Render the template, writing to `w`.
+	t.Execute(w, "Duder")
+
+	// Done.
+	log.Println(r.RemoteAddr, "Finished HTTP request at", r.URL.Path)
+}
+
+// Main routine
+//
+func main() {
+	addr := ":8000"
+
+	// Connect to MongoDB
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	session.SetMode(mgo.Monotonic, true) // Optional. Switch the session to a monotonic behavior.
+	sse := &SSE{session}
+
+	http.Handle("/events/", sse)
+	http.Handle("/", http.HandlerFunc(MainPageHandler))
+
+	log.Println("Listening at", addr)
+	http.ListenAndServe(addr, nil)
+}
---
+++ pillar/notifications.go
@@ -0,0 +1,56 @@
+package pillar
+
+import (
+	"time"
+	"gopkg.in/mgo.v2/bson"
+	"gopkg.in/mgo.v2"
+	"fmt"
+)
+
+type Notification struct {
+	Id       bson.ObjectId `bson:"_id"`
+	Created  time.Time     `bson:"_created"`
+	Updated  time.Time     `bson:"_updated"`
+	User     bson.ObjectId `bson:"user"`
+	Activity bson.ObjectId `bson:"activity"`
+	Etag     string        `bson:"_etag"`
+}
+
+
+func ForwardNotifications(session *mgo.Session) chan *Notification {
+	ch := make(chan *Notification)
+
+	go func() {
+		my_sess := session.Copy()
+		defer my_sess.Close()
+
+		notifications := my_sess.DB("eve").C("notifications")
+		result := Notification{}
+		var last_seen time.Time
+		var query bson.M
+
+		for {
+			// Fetch notifications from MongoDB.
+			query = bson.M{"_created": bson.M{"$gt": last_seen}}
+			iter := notifications.Find(query).Sort("_created").Iter()
+
+			// Send notifications to the client.
+			for iter.Next(&result) {
+				last_seen = result.Created
+				ch <- &result
+			}
+
+			if err := iter.Close(); err != nil {
+				fmt.Println("Error fetching notifications", err)
+				close(ch)
+				return
+			}
+
+			time.Sleep(5 * time.Second)
+		}
+
+		close(ch)
+	}()
+
+	return ch
+}
