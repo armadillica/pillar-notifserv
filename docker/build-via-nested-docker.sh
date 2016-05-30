76fd639dd273423b9d0ae4a62f866aa3cafb5793
Sybren A. Stüvel <sybren@stuvel.eu>
2016-05-30 08:37:54 +0200

    Docker: tag with git hash and export container to tgz

--- docker/build-via-nested-docker.sh
+++ docker/build-via-nested-docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+HASH=$(git show-ref --head --hash HEAD)
+EXPORT_TO=pillar-notifserv-${HASH}.docker.tgz
+
 # Use Docker to get Go in a way that allows overwriting the
 # standard library with statically linked versions.
 docker run -i --rm \
@@ -12,11 +15,17 @@ cp \${GOPATH}/bin/pillar-notifserv /docker
 EOT
 
 # Use the statically linked executable to build our final Docker image.
-docker build -t armadillica/pillar-notifserv .
+docker build -t armadillica/pillar-notifserv:${HASH} .
 
+if docker ps -a --no-trunc | grep -q notifserv; then
+    echo
+    echo '==> Docker container "notifserv" already exists, press ENTER to remove and recreate.'
+    read dummy
+    docker stop notifserv
+    docker rm notifserv
+fi
+
+docker create --name notifserv --net host  armadillica/pillar-notifserv:${HASH}
+docker export notifserv | gzip > ${EXPORT_TO}
 echo
-echo Docker image is build. Now you can run it with:
-echo docker run --rm --name notifserv --net host  armadillica/pillar-notifserv
-echo
-echo Or create a new container using:
-echo docker create --name notifserv --net host  armadillica/pillar-notifserv
+echo Docker container created and exported to ${EXPORT_TO}
