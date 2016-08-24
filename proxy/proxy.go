42e9ad178764c2629a0882bccbd7a7d205aa86eb
Sybren A. Stüvel <sybren@stuvel.eu>
2016-08-24 18:29:21 +0200

    Prevent double URL in proxy target
    Without this hack of using a / URL, proxied requests are doubled, i.e.
    a request to /jemoeder will be sent to
    http://proxytarget/jemoeder/jemoeder.

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -2,9 +2,11 @@ package proxy
 
 import (
 	"net/http"
-
+	"net/url"
 	"github.com/armadillica/pillar-notifserv/pillar"
 )
+
+var root_url, _ = url.Parse("/")
 
 // Proxy is a dynamic reverse proxy.
 type Proxy struct {
@@ -30,7 +32,10 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	url := p.cfg.Target.ResolveReference(r.URL)
+	// Without this hack of using a / URL, proxied requests
+	// are doubled, i.e. a request to /jemoeder will be sent
+	// to http://proxytarget/jemoeder/jemoeder.
+	url := p.cfg.Target.ResolveReference(root_url)
 	var h http.Handler
 	switch {
 	case r.Header.Get("Upgrade") == "websocket":
